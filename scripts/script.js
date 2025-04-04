6af61dfc4bede5ff678c9cf337fa1887c95e4644
Sebastiaan <sebastiaan.hagoort@hva.nl>
2025-04-04 10:06:01 +0200

    vragenlijst update sprint 9

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+console.log('test')
+
+if (audioSupported ) {
+  
+  var audioT = document.getElementById("audio");
+  audioT.style.display = "none";
+
+  function playAudio() {
+      audioT.play();
+  }
+  function pauseAudio() {
+      audioT.pause();
+  }
+
+
+  //  use muted to pause music
+//   function muteAudio() {
+//       audioT.muted = !audioT.muted;
+
+button.hidden = false;
+
+audio.hidden = true;
+}
